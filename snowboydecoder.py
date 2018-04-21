65bfdeccfea39c723780bdffc1867efb8eae98e4
hellozee <hellozee@disroot.org>
2018-04-21 17:00:18 +0530

    added text to speech

--- snowboydecoder.py
+++ snowboydecoder.py
@@ -9,6 +9,7 @@ import os
 import logging
 from ctypes import *
 from contextlib import contextmanager
+import stt
 
 logging.basicConfig()
 logger = logging.getLogger("snowboy")
@@ -61,20 +62,7 @@ def play_audio_file(fname=DETECT_DING):
     :param str fname: wave file name
     :return: None
     """
-    ding_wav = wave.open(fname, 'rb')
-    ding_data = ding_wav.readframes(ding_wav.getnframes())
-    with no_alsa_error():
-        audio = pyaudio.PyAudio()
-    stream_out = audio.open(
-        format=audio.get_format_from_width(ding_wav.getsampwidth()),
-        channels=ding_wav.getnchannels(),
-        rate=ding_wav.getframerate(), input=False, output=True)
-    stream_out.start_stream()
-    stream_out.write(ding_data)
-    time.sleep(0.2)
-    stream_out.stop_stream()
-    stream_out.close()
-    audio.terminate()
+    stt.play('resources/audio/yes.wav')
 
 
 class HotwordDetector(object):
--- stt.py
+++ stt.py
@@ -1,9 +1,43 @@
 #!/usr/bin/python3
 from wit import Wit
 import os
+import talkey
 
 client = Wit(os.environ['WIT'])
-resp = None
-with open('test.wav', 'rb') as f:
-  resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
-print('Yay, got Wit.ai response: ' + str(resp))
+
+def gettext():
+	resp = None
+	with open('test.wav', 'rb') as f:
+		resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
+	return resp
+
+def say(text):
+	import json
+	from watson_developer_cloud import TextToSpeechV1
+
+	text_to_speech = TextToSpeechV1(
+    	username=os.environ['WATSON_USER'],
+    	password=os.environ['WATSON_PASS'])
+
+	with open('output.wav','wb') as audio_file:
+		audio_file.write( text_to_speech.synthesize(text, accept='audio/wav',voice="en-US_AllisonVoice").content)
+	play('output.wav')
+
+def play(filename):
+	import wave, sys, pyaudio
+	wf = wave.open(filename)
+	p = pyaudio.PyAudio()
+	chunk = 1024
+	stream = p.open(format =
+					p.get_format_from_width(wf.getsampwidth()),
+					channels = wf.getnchannels(),
+					rate = wf.getframerate(),
+					output = True)
+	data = wf.readframes(chunk)
+	while data:
+		stream.write(data)
+		data = wf.readframes(chunk)
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
