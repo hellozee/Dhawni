19fdab54c595d1ebe7b3136b4a4c99e0064ce3c3
hellozee <hellozee@disroot.org>
2018-04-22 06:14:21 +0530

    prepared for merge

--- analyze.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from textblob import TextBlob
-
-string = TextBlob("This is my birthday!")
-print(TextBlob.tokenize(string))
-print(string.parse())
--- orca/src/orca/scripts/toolkits/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['clutter',
-           'gtk',
-           'GAIL',
-           'Gecko',
-           'J2SE-access-bridge',
-           'Qt',
-           'VCL.py',
-           'WebKitGtk']
