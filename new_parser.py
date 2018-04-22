5a9846bf2deb8f11ba8cd5916eb76ae62cae2b22
emht <emht@gitlab.com>
2018-04-22 10:35:11 +0530

    Update

--- new_parser.py
+++ new_parser.py
@@ -25,15 +25,19 @@ attributes = []
 blob = TextBlob(text)
 token_string = blob.words
 mood = blob.sentiment.polarity
-print(mood)
+#print(mood)
+found = False
 
 e = 0.005 # To set range for the neutral mood.
 
 for index in range(len(token_string)):
 	if token_string[index] in commands:
+		found = True;
+		print(token_string[index])
 		#Command found and look for the target
 		for ahead in range(index, len(token_string)):
 			if token_string[ahead] in actions:
+				print(token_string[ahead])
 				# Call the execution function for (command, action)
 				# Update the index with ahead + 1
 				print("Add the execute command function");
@@ -41,6 +45,10 @@ for index in range(len(token_string)):
 				if token_string[ahead] == 'brightness' or 'volume':
 					# If Brightness and volume, look for third attribute
 					pass
+	elif index == len(token_string) and found == False:
+		# Invalid input. Say, "Can you rephrase your words?"
+		print("Can you please rephrase your words?")
+
 if -1 <=  mood <= (0 - e):
 	# Bad mood. Call function to cheer him/her up. Songs/jokes
 	print("Call the mood cheerer man!")
@@ -50,8 +58,5 @@ if (0 + e) <=  mood <= 1:
 if (0 - e) <=  mood <= (0 + e):
 	# Neutral sentiment output. Make a pun joke
 	print("Neutral Sentiment")
-#else:
-#	# Invalid input. Say, "Can you rephrase your words?"
-#	print("Can you please rephrase your words?")
 	
 
